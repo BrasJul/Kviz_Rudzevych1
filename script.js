544b1314d601af9555818e31f752ae30cb454428
BrasJul <logikaulia@gmail.com>
2026-02-07 16:55:18 +0200

    Add translation questions for animals and literature

---
+++ script.js
@@ -0,0 +1,83 @@
+const basicsQuestions = [
+    {
+        question: "Translate: 'Dog'",
+        answers: ["Кіт", "Собака", "Птах", "Миша"],
+        correct: 1
+    },
+    {
+        question: "Translate: 'Cat'",
+        answers: ["Кінь", "Кіт", "Корова", "Риба"],
+        correct: 1
+    },
+    {
+        question: "Translate: 'Bird'",
+        answers: ["Птах", "Жаба", "Змія", "Вовк"],
+        correct: 0
+    },
+    {
+        question: "Translate: 'Fish'",
+        answers: ["Миша", "Риба", "Качка", "Кролик"],
+        correct: 1
+    },
+    {
+        question: "Translate: 'Horse'",
+        answers: ["Слон", "Кінь", "Тигр", "Ведмідь"],
+        correct: 1
+    }
+];
+
+const phrasesQuestions = [
+    {
+        question: "Translate: 'Cow'",
+        answers: ["Кінь", "Корова", "Вівця", "Кіт"],
+        correct: 1
+    },
+    {
+        question: "Translate: 'Sheep'",
+        answers: ["Коза", "Свиня", "Вівця", "Курка"],
+        correct: 2
+    },
+    {
+        question: "Translate: 'Pig'",
+        answers: ["Свиня", "Заєць", "Вовк", "Олень"],
+        correct: 0
+    },
+    {
+        question: "Translate: 'Duck'",
+        answers: ["Гуска", "Качка", "Курка", "Птах"],
+        correct: 1
+    },
+    {
+        question: "Translate: 'Rabbit'",
+        answers: ["Кіт", "Миша", "Кролик", "Лисиця"],
+        correct: 2
+    }
+];
+
+const literatureQuestions = [
+    {
+        question: "Translate: 'Lion'",
+        answers: ["Тигр", "Лев", "Ведмідь", "Вовк"],
+        correct: 1
+    },
+    {
+        question: "Translate: 'Tiger'",
+        answers: ["Леопард", "Лев", "Тигр", "Рись"],
+        correct: 2
+    },
+    {
+        question: "Translate: 'Bear'",
+        answers: ["Вовк", "Лисиця", "Ведмідь", "Олень"],
+        correct: 2
+    },
+    {
+        question: "Translate: 'Wolf'",
+        answers: ["Собака", "Вовк", "Лисиця", "Кіт"],
+        correct: 1
+    },
+    {
+        question: "Translate: 'Fox'",
+        answers: ["Вовк", "Кіт", "Лисиця", "Миша"],
+        correct: 2
+    }
+];
